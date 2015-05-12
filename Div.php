b75c55364bc34759b63ae2a38e8bf5ad98610e52
Tayron Miranda <dev@tayron.com.br>
2015-05-12 17:41:32 -0300

    Adicionando documentação na classe.

--- Div.php
+++ Div.php
@@ -1,14 +1,23 @@
 <?php
 
 /**
- * Div class
+ * Classe que renderiza um elemento div.
+ * Com este classe é possível definir qualquer atributo que o elemento deverá ter.
+ * 
+ * @author: Tayron Miranda <dev@tayron.com.br>
  */
 class Div{
     private $elements = array();
     private $atributes = array();
 
     /**
-     *
+     * Método construtor da classe, recebe o conteúdo que a div irá conter e um 
+     * array de atributos que a div deverá possuir.
+     * 
+     * @access public
+     * @param mixed $element Elemento que a div irá conter
+     * @param array $atributes Array com os atributos que a div irá ter.
+     * @return void
      */
     public function __construct( $element, $atributes = array() )
     {
@@ -20,7 +29,10 @@ class Div{
     }
 
     /**
-     *
+     * Método que renderiza a div na tela.
+     * 
+     * @acess public
+     * @return string
      */
     public function display()
     {
@@ -36,7 +48,10 @@ class Div{
     }
 
     /**
-     *
+     * Método que monta e retorna os atibutos que a div irá ter.
+     * 
+     * @access private 
+     * @return string
      */
     private function getAtributes()
     {
